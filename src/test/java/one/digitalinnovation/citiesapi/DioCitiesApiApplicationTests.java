75ab92070ded4c0b450cd4682bfad26ebacff538
Romualdo <romualdoandre@gmail.com>
2021-10-13 22:17:38 -0300

    projeto inicial

---
+++ src/test/java/one/digitalinnovation/citiesapi/DioCitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioCitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
